f52d8617a99ae15db247839f6ff4103c3bb4a16e
Oskar Laulajainen <oskar.laulajainen.syne23lin@edu.tucsweden.se>
2024-08-16 13:49:04 +0200

    Adjusted form for better visibility and fixed tab index

--- Labb1/Form1.Designer.cs
+++ Labb1/Form1.Designer.cs
@@ -40,36 +40,40 @@
             // 
             // txtInput
             // 
-            txtInput.Location = new Point(37, 58);
+            txtInput.Location = new Point(32, 44);
+            txtInput.Margin = new Padding(3, 2, 3, 2);
             txtInput.Multiline = true;
             txtInput.Name = "txtInput";
-            txtInput.Size = new Size(262, 87);
+            txtInput.Size = new Size(230, 66);
             txtInput.TabIndex = 0;
             // 
             // txtOutput
             // 
-            txtOutput.Location = new Point(37, 195);
+            txtOutput.Location = new Point(32, 146);
+            txtOutput.Margin = new Padding(3, 2, 3, 2);
             txtOutput.Multiline = true;
             txtOutput.Name = "txtOutput";
-            txtOutput.Size = new Size(262, 87);
+            txtOutput.Size = new Size(230, 66);
             txtOutput.TabIndex = 1;
             // 
             // btnEncrypt
             // 
-            btnEncrypt.Location = new Point(37, 382);
+            btnEncrypt.Location = new Point(32, 286);
+            btnEncrypt.Margin = new Padding(3, 2, 3, 2);
             btnEncrypt.Name = "btnEncrypt";
-            btnEncrypt.Size = new Size(77, 27);
-            btnEncrypt.TabIndex = 2;
+            btnEncrypt.Size = new Size(67, 30);
+            btnEncrypt.TabIndex = 3;
             btnEncrypt.Text = "Encrypt";
             btnEncrypt.UseVisualStyleBackColor = true;
             btnEncrypt.Click += btnEncrypt_Click;
             // 
             // btnDecrypt
             // 
-            btnDecrypt.Location = new Point(224, 381);
+            btnDecrypt.Location = new Point(196, 286);
+            btnDecrypt.Margin = new Padding(3, 2, 3, 2);
             btnDecrypt.Name = "btnDecrypt";
-            btnDecrypt.Size = new Size(75, 28);
-            btnDecrypt.TabIndex = 3;
+            btnDecrypt.Size = new Size(66, 30);
+            btnDecrypt.TabIndex = 4;
             btnDecrypt.Text = "Decrypt";
             btnDecrypt.UseVisualStyleBackColor = true;
             btnDecrypt.Click += btnDecrypt_Click;
@@ -77,43 +81,44 @@
             // lblInput
             // 
             lblInput.AutoSize = true;
-            lblInput.Location = new Point(37, 35);
+            lblInput.Location = new Point(32, 26);
             lblInput.Name = "lblInput";
-            lblInput.Size = new Size(43, 20);
-            lblInput.TabIndex = 4;
+            lblInput.Size = new Size(35, 15);
+            lblInput.TabIndex = 5;
             lblInput.Text = "Input";
             // 
             // lblOutput
             // 
             lblOutput.AutoSize = true;
-            lblOutput.Location = new Point(37, 172);
+            lblOutput.Location = new Point(32, 129);
             lblOutput.Name = "lblOutput";
-            lblOutput.Size = new Size(55, 20);
-            lblOutput.TabIndex = 5;
+            lblOutput.Size = new Size(45, 15);
+            lblOutput.TabIndex = 6;
             lblOutput.Text = "Output";
             // 
             // txtPassword
             // 
-            txtPassword.Location = new Point(37, 326);
+            txtPassword.Location = new Point(32, 244);
+            txtPassword.Margin = new Padding(3, 2, 3, 2);
             txtPassword.Name = "txtPassword";
             txtPassword.PasswordChar = '*';
-            txtPassword.Size = new Size(262, 27);
-            txtPassword.TabIndex = 6;
+            txtPassword.Size = new Size(230, 23);
+            txtPassword.TabIndex = 2;
             // 
             // lblPassword
             // 
             lblPassword.AutoSize = true;
-            lblPassword.Location = new Point(37, 303);
+            lblPassword.Location = new Point(32, 227);
             lblPassword.Name = "lblPassword";
-            lblPassword.Size = new Size(70, 20);
+            lblPassword.Size = new Size(57, 15);
             lblPassword.TabIndex = 7;
             lblPassword.Text = "Password";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(335, 450);
+            ClientSize = new Size(293, 338);
             Controls.Add(lblPassword);
             Controls.Add(txtPassword);
             Controls.Add(lblOutput);
@@ -122,6 +127,7 @@
             Controls.Add(btnEncrypt);
             Controls.Add(txtOutput);
             Controls.Add(txtInput);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
